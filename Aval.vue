ce4e42979235dd28ffa311f6b02e9397ca482d65
Xaobin <40346917+Xaobin@users.noreply.github.com>
2023-04-06 23:18:24 -0400

    Update Aval.vue

--- Aval.vue
+++ Aval.vue
@@ -1,29 +1,54 @@
 <template>
-  <div>
+<div class="container col-md-6">
+<span><a href="https://github.com/Xaobin" target="_blank"><small class="smll">{{per}}</small></a></span>
+<span>&nbsp;&nbsp;&nbsp;<div class="arrow-up ml-2"></div></span>
+<div class="alert alert-info alert-dismissible fade show" v-if="inShow">
+
+    {{msg}} <a :href="'#'+lnk">{{lnk}}</a>
     
-    
-    <CselComp />
-   
-  </div>
+</div>
+</div>
 </template>
 <script>
 /* eslint-disable */
-import dtt from '@/lang.json'
-import CselComp from '@/components/Csel.vue'
   export default {
-  
+  data(){
+        return {
+        inShow:true
+   }},     
+   props: {
+     msg: String,
+     lnk:String,
+     per:String
+   }, 
    name: 'AvalComp',
    components: {
-    CselComp
+    
   },
     
     methods:{
     
   },
   mounted(){
-     console.log(dtt[0].a);
+     
   }
 
 }
 
 </script>
+<style scoped>
+.arrow-up {
+  width: 0; 
+  height: 0; 
+  border-left: 15px solid transparent;
+  border-right: 15px solid transparent;
+  
+  border-bottom: 15px solid #D6EBFD;
+}
+.smll{
+color:black;
+font-size:10px;
+text-decoration:underline;
+}
+
+</style>
