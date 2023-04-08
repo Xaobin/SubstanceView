5f4a1e13f9cf2c7d320d90953d207ad56cbe4389
Xaobin <40346917+Xaobin@users.noreply.github.com>
2023-04-07 22:36:44 -0400

    Add files via upload

---
+++ Footer.vue
@@ -0,0 +1,206 @@
+<template>
+<div>
+<p class="bg-dark p-1">
+    <section class="p-4">
+      <!-- Footer -->
+      <footer class="bg-dark text-center text-white">
+        <!-- Grid container -->
+        <div class="container p-4">
+          <!-- Section: Social media -->
+          <section class="mb-4">
+            <!-- Facebook -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+              ><i class="fab fa-facebook-f"></i
+            ></a>
+
+            <!-- Twitter -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+              ><i class="fab fa-twitter"></i
+            ></a>
+
+            <!-- Google -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+              ><i class="fab fa-google"></i
+            ></a>
+
+            <!-- Instagram -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+              ><i class="fab fa-instagram"></i
+            ></a>
+
+            <!-- Linkedin -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+              ><i class="fab fa-linkedin-in"></i
+            ></a>
+
+            <!-- Github -->
+            <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+              ><i class="fab fa-github"></i
+            ></a>
+          </section>
+          <!-- Section: Social media -->
+
+          <!-- Section: Form -->
+          <section class="">
+            <form action="">
+              <!--Grid row-->
+              <div class="row d-flex justify-content-center">
+                <!--Grid column-->
+                <div class="col-auto">
+                  <p class="pt-2">
+                    <strong>Sign up for our newsletter</strong>
+                  </p>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-5 col-12">
+                  <!-- Email input -->
+                  <div class="form-outline form-white mb-4">
+                    <input type="email" id="form5Example24" class="form-control" />
+                    <label class="form-label" for="form5Example24">Email address</label>
+                  </div>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-auto">
+                  <!-- Submit button -->
+                  <button type="submit" class="btn btn-outline-light mb-4">
+                    Subscribe
+                  </button>
+                </div>
+                <!--Grid column-->
+              </div>
+              <!--Grid row-->
+            </form>
+          </section>
+          <!-- Section: Form -->
+
+          <!-- Section: Text -->
+          <section class="mb-4">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+              repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam eum
+              harum corrupti dicta, aliquam sequi voluptate quas.
+            </p>
+          </section>
+          <!-- Section: Text -->
+
+          <!-- Section: Links -->
+          <section class="">
+            <!--Grid row-->
+            <div class="row">
+              <!--Grid column-->
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 target="_blank" class="text-uppercase">Front End</h5>
+
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="https://php.net" target="_blank" class="text-white">php.net</a>
+                  </li>
+                  <li>
+                    <a href="https://laravel.com/docs/10.x" target="_blank" class="text-white">Laravel</a>
+                  </li>
+                  <li>
+                    <a href="https://codeigniter.com" target="_blank" class="text-white">CodeIgniter</a>
+                  </li>
+                  <li>
+                    <a href="https://laravel-excel.com" target="_blank" class="text-white">Laravel Excel</a>
+                  </li>
+                </ul>
+              </div>
+              <!--Grid column-->
+
+              <!--Grid column-->
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 target="_blank" class="text-uppercase">Back End</h5>
+
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="https://vuejs.org" target="_blank" class="text-white">Vuejs</a>
+                  </li>
+                  <li>
+                    <a href="https://react.dev" target="_blank" class="text-white">React</a>
+                  </li>
+                  <li>
+                    <a href="https://npmjs.com" target="_blank" class="text-white">npm software</a>
+                  </li>
+                  <li>
+                    <a href="https://www.typescriptlang.org/" target="_blank" class="text-white">Typescript</a>
+                  </li>
+                </ul>
+              </div>
+              <!--Grid column-->
+
+              <!--Grid column-->
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 target="_blank" class="text-uppercase">Links</h5>
+
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="https://github.com/" target="_blank" class="text-white">Github</a>
+                  </li>
+                  <li>
+                    <a href="https://getbootstrap.com/" target="_blank" class="text-white">Bootstrap</a>
+                  </li>
+                  <li>
+                    <a href="https://vscodium.com/" target="_blank" class="text-white">VSCodium</a>
+                  </li>
+                  <li>
+                    <a href="https://bitnami.com/" target="_blank" class="text-white">Bitnami</a>
+                  </li>
+                </ul>
+              </div>
+              <!--Grid column-->
+
+              <!--Grid column-->
+              <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 target="_blank" class="text-uppercase">Links</h5>
+
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="https://www.convergenciadigital.com.br/" target="_blank" class="text-white">Convergência digital</a>
+                  </li>
+                  <li>
+                    <a href="https://openai.com/blog/chatgpt" target="_blank" class="text-white">ChatGPT</a>
+                  </li>
+                  <li>
+                    <a href="https://distrowatch.com/?language=PT" target="_blank" class="text-white">Distro watch</a>
+                  </li>
+                  <li>
+                    <a href="https://bard.google.com/" target="_blank" class="text-white">Google Bard</a>
+                  </li>
+                </ul>
+              </div>
+              <!--Grid column-->
+            </div>
+            <!--Grid row-->
+          </section>
+          <!-- Section: Links -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div target="_blank" class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+          © 2023 
+        </div>
+        <!-- Copyright -->
+      </footer>
+      <!-- Footer -->
+    </section>
+
+</p>
+</div>
+</template>
+<script>
+/* eslint-disable */
+  export default {
+    name:'FooterComp'
+
+}
+
+</script>
+<style scoped>
+
+</style>
